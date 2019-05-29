9c3ad0211060ba4be09a02afef2ed38cde085c67
MatiasBoldini <matiboldini2000@gmail.com>
2019-05-29 09:41:32 -0300

    Funciones de js, basicas, gradosradianes y div

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -24,7 +24,40 @@ function convertir(id_elem, valor_elem) {
             document.getElementById("pulgada").value = valor_elem*39.3701;
         }
         if(id_elem==''){
-
         }
     }
-} 
+}
+
+function suma(){
+    document.opeMat.sum_total.value = Number(document.opeMat.sum_num1.value) + Number(document.opeMat.sum_num2.value) 
+}
+
+function resta(){
+    document.opeMat.res_total.value = Number(document.opeMat.res_num1.value) - Number(document.opeMat.res_num2.value)
+}
+
+function mult(){
+    document.opeMat.mul_total.value = Number(document.opeMat.mul_num1.value) * Number(document.opeMat.mul_num2.value)
+}
+
+function divi(){
+    document.opeMat.div_total.value = Number(document.opeMat.div_num1.value) / Number(document.opeMat.div_num2.value)
+}
+
+function gradosRadianes(id_elem, valor){
+    if(id_elem=="grados"){
+        document.getElementById("radianes").value = valor*Math.PI/180;
+    }
+    if(id_elem=="radianes"){
+        document.getElementById("grados").value = valor*180/Math.PI;
+    }
+}
+
+function mostrar_ocultar(id_elem){
+    if(id_elem=="ocultarDiv"){
+        document.getElementById("unDiv").style.display = 'none';
+    }
+    if(id_elem=="mostrarDiv"){
+        document.getElementById("unDiv").style.display = 'block';
+    }
+}
